a13fe17fffcb6915769d95bdd3ea6a097d740fa2
Thiago Spart <user000.000resu@gmail.com>
2021-01-27 23:24:53 -0300

    att1

---
+++ pages/room/src/view.js
@@ -0,0 +1,42 @@
+class View {
+	constructor() {
+
+	}
+
+	creatVideoElement({ muted = true, src, srcObject }) {
+		const video = document.createElement('video')
+		video.muted = muted
+		video.src = src
+		video.srcObject = srcObject
+
+		if (src) {
+			video.controls = true
+			video.loop = true  
+			Util.sleep(200).then(_ => video.play())
+		}
+
+		if (srcObject) {
+			video.addEventListener("loadedmetadata", _ => video.play())
+		}
+
+		return video
+	}
+
+	renderVideo({ userId, stream = null, url = null, isCurrentId = false}) {
+		const video = this.creatVideoElement({src: url, srcObject: stream})
+		this.appendToHTMLTree(userId, video, isCurrentId)
+	}
+
+	appendToHTMLTree(userId, video, isCurrentId) {
+		const div = document.createElement('div')
+		div.id = userId
+		div.classList.add('wrapper')
+		div.append(video)
+		const div2 = document.createElement('div')
+		div2.innerText = isCurrentId ? '' : userId
+		div2.append(video)
+
+		const videoGrid = document.getElementById('video-grid')
+		videoGrid.append(div)
+	}
+}
